2eb52602d1477386d0dc5d7e4179614d79e0024b
Igor Segalla <igorsegallafa@gmail.com>
2019-10-31 03:27:38 -0300

    Source Files

---
+++ Source/Game/PrecompiledHeader.h
@@ -0,0 +1,13 @@
+#pragma once
+
+//STD:
+#include <string>
+#include <sstream>
+
+//Urho3D:
+#include <Urho3D/Engine/Application.h>
+#include <Urho3D/Engine/Engine.h>
+#include <Urho3D/Engine/EngineDefs.h>
+#include <Urho3D/Input/Input.h>
+
+using namespace Urho3D;
