78d8eb1bbfd6e64cdae6849a18947542d76e0163
Igor Segalla <36571229+igorsegallafa@users.noreply.github.com>
2020-01-08 20:54:51 -0300

    NoesisGUI implementation

--- Source/Game/Main.h
+++ Source/Game/Main.h
@@ -17,6 +17,38 @@ public:
 
     //! Stop Engine.
     void Stop();
+
+    //! Post Update Handler.
+    void HandlePostUpdate( StringHash eventType, VariantMap& eventData );
+
+    //! Render GUI Handler.
+    void HandleRender( StringHash eventType, VariantMap& eventData );
+
+    //! Window Resized Handler.
+    void HandleWindowResized( StringHash eventType, VariantMap& eventData );
+
+    //! Mouse Button Down Handler.
+    void HandleMouseButtonDown( StringHash eventType, VariantMap& eventData );
+
+    //! Mouse Button Up Handler.
+    void HandleMouseButtonUp( StringHash eventType, VariantMap& eventData );
+
+    //! Mouse Move Handler.
+    void HandleMouseMove( StringHash eventType, VariantMap& eventData );
+
+    //! Mouse Wheel Handler.
+    void HandleMouseWheel( StringHash eventType, VariantMap& eventData );
+
+    //! Key Down Handler.
+    void HandleKeyDown( StringHash eventType, VariantMap& eventData );
+
+    //! Key Up Handler.
+    void HandleKeyUp( StringHash eventType, VariantMap& eventData );
+
+    //! Key Char Handler.
+    void HandleKeyChar( StringHash eventType, VariantMap& eventData );
+private:
+    Noesis::IView* view;
 };
 
 URHO3D_DEFINE_APPLICATION_MAIN( Game )
--- Source/Game/PrecompiledHeader.h
+++ Source/Game/PrecompiledHeader.h
@@ -5,6 +5,13 @@
 #include <sstream>
 #include <functional>
 
+//NoesisGUI:
+#include <NsRender/GLFactory.h>
+#include <NsGui/IntegrationAPI.h>
+#include <NsGui/IRenderer.h>
+#include <NsGui/IView.h>
+#include <NsGui/Grid.h>
+
 //Urho3D:
 #include <Urho3D/Urho3DAll.h>
 
